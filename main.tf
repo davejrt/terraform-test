fe7c10b2f473897a4abaf9c1c986b8780460c6cc
Dave Try <dave.try@puppet.com>
2019-06-06 10:38:36 -0500

    testing atlantis

--- main.tf
+++ main.tf
@@ -8,4 +8,12 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "null_resource" "example" {}
+resource "aws_instance" "web2" {
+  ami           = "ami-005bdb005fb00e791"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name     = "HelloWorld"
+    lifetime = "1h"
+  }
+}
